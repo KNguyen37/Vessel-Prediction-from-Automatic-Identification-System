0ce98b242e4da916c7738e4f2499d0b054b705fc
anhtu-pham <tap95@case.edu>
2023-11-29 21:44:01 -0500

    Write code for building affinity matrix

--- predictVessel.py
+++ predictVessel.py
@@ -11,6 +11,9 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import adjusted_rand_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics import pairwise_kernels
+import math
 
 def predictWithK(testFeatures, numVessels, trainFeatures=None, 
                  trainLabels=None):
@@ -18,11 +21,60 @@ def predictWithK(testFeatures, numVessels, trainFeatures=None,
     
     scaler = StandardScaler()
     testFeatures = scaler.fit_transform(testFeatures)
-    km = KMeans(n_clusters=numVessels, init='k-means++', n_init=10, 
-                random_state=100)
-    predVessels = km.fit_predict(testFeatures)
-    
-    return predVessels
+    # km = KMeans(n_clusters=numVessels, init='k-means++', n_init=10, 
+    #             random_state=100)
+    # predVessels = km.fit_predict(testFeatures)
+
+    def preprocess_data(data):
+        # Sort data based on timestamps
+        data = data[data[:, 2].argsort()]
+
+        # Remove duplicate data with extremely close coordinates and courses over ground at the same time
+        unique_data = []
+        for i in range(len(data) - 1):
+            if not np.allclose(data[i, 3:], data[i + 1, 3:], atol=1e-6):
+                unique_data.append(data[i])
+        unique_data.append(data[-1])
+        unique_data = np.array(unique_data)
+
+        return unique_data
+
+    def construct_affinity_graph(unique_data):
+        num_points = len(unique_data)
+        affinity_matrix = np.zeros((num_points, num_points))
+
+        for i in range(num_points):
+            for j in range(i + 1, num_points):
+                t1, x1, y1, v1, theta1 = unique_data[i, :5]
+                t2, x2, y2, v2, theta2 = unique_data[j, :5]
+
+                x2_pred = x1 + v1 * math.cos(theta1) * (t2 - t1)
+                y2_pred = y2 + v1 * math.sin(theta1) * (t2 - t1)
+
+                # Calculate similarity using Gaussian similarity function
+                similarity = pairwise_kernels([[x2_pred, y2_pred]], [[x2, y2]], metric='rbf')[0, 0]
+
+                # Calculate tolerance proportional to distance
+                # distance = euclidean_distances([[x1, y1]], [[x2, y2]])[0, 0]
+                # tolerance = 1.0 / (1.0 + distance)
+
+                # Construct affinity graph
+                affinity_matrix[i, j] = affinity_matrix[j, i] = similarity
+
+        return affinity_matrix
+
+
+    # Assuming testFeatures is the preprocessed unique data
+    affinity_matrix = construct_affinity_graph(testFeatures)
+
+    # Perform clustering using some clustering algorithm (e.g., KMeans)
+    # You may need to install scikit-learn if you haven't already: pip install scikit-learn
+    from sklearn.cluster import KMeans
+
+    kmeans = KMeans(n_clusters=numVessels)
+    pred_labels = kmeans.fit_predict(affinity_matrix)
+
+    return pred_labels
 
 def predictWithoutK(testFeatures, trainFeatures=None, trainLabels=None):
     # Unsupervised prediction, so training data is unused
@@ -38,12 +90,12 @@ if __name__ == "__main__":
     features = data[:,2:]
     labels = data[:,1]
 
-    #%% Plot all vessel tracks with no coloring
+    # Plot all vessel tracks with no coloring
     plotVesselTracks(features[:,[2,1]])
     plt.title('All vessel tracks')
     
-    #%% Run prediction algorithms and check accuracy
-    
+    # Run prediction algorithms and check accuracy
+
     # Prediction with specified number of vessels
     numVessels = np.unique(labels).size
     predVesselsWithK = predictWithK(features, numVessels)
@@ -58,7 +110,7 @@ if __name__ == "__main__":
     print(f'Adjusted Rand index for estimated K = {predNumVessels}: '
           + f'{ariWithoutK}')
 
-    #%% Plot vessel tracks colored by prediction and actual labels
+    # Plot vessel tracks colored by prediction and actual labels
     plotVesselTracks(features[:,[2,1]], predVesselsWithK)
     plt.title('Vessel tracks by cluster with K')
     plotVesselTracks(features[:,[2,1]], predVesselsWithoutK)
