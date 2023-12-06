16704ccb9f749a8b1ecd6ef47c3a878040242f43
anhtu-pham <tap95@case.edu>
2023-12-05 21:28:18 -0500

    Try extracting features for K-Means, use distances for hierarchical clustering

--- predictVessel.py
+++ predictVessel.py
@@ -7,19 +7,21 @@ number of vessels is not specified, assume 20 vessels.
 """
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import adjusted_rand_score
 from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans, SpectralClustering
-from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.cluster import KMeans, SpectralClustering, AgglomerativeClustering
+from scipy.cluster.hierarchy import linkage, fcluster
+from sklearn.metrics.pairwise import haversine_distances, euclidean_distances
+from scipy.spatial.distance import euclidean, squareform
 from sklearn.metrics.pairwise import rbf_kernel
 from sklearn.preprocessing import MinMaxScaler
 import math
 
-def predictWithK(testFeatures, numVessels, s, trainFeatures=None, 
+def predictWithK(testFeatures, numVessels, trainFeatures=None, 
                  trainLabels=None):
     # Unsupervised prediction, so training data is unused
-    
     # scaler = StandardScaler()
     # testFeatures = scaler.fit_transform(testFeatures)
     # km = KMeans(n_clusters=numVessels, init='k-means++', n_init=10, 
@@ -27,33 +29,17 @@ def predictWithK(testFeatures, numVessels, s, trainFeatures=None,
     # predVessels = km.fit_predict(testFeatures)
     # return predVessels
 
+
+
+
     def preprocess_data(data):
         # Sort data based on timestamps
-        data = data[data[:, 2].argsort()]
+        # data = data[data[:, 2].argsort()]
         return data
 
-    def construct_affinity_matrix(data, s):
+    def construct_distance_matrix(data):
         num_points = len(data)
         distances = np.zeros((num_points, num_points))
-
-        # for i in range(num_points):
-        #     t1, x1, y1, v1, theta1 = unique_data[i, :]
-
-        #     later_time_indices = np.where(unique_data[:, 0] > t1)[0]
-        #     later_data = unique_data[later_time_indices, :]
-        #     time_diff = later_data[:, 0] - t1
-
-        #     acc_approx = np.where(time_diff != 0, (later_data[:, 3] - v1) / time_diff, 0)
-        #     x2_pred = x1 + np.cos(theta1) * (v1 * time_diff + 0.5 * acc_approx * time_diff**2)
-        #     y2_pred = y1 + np.sin(theta1) * (v1 * time_diff + 0.5 * acc_approx * time_diff**2)
-
-        #     distances = np.sqrt((x2_pred - later_data[:, 1])**2 + (y2_pred - later_data[:, 2])**2)
-        #     affinity_matrix[i, later_time_indices] = rbf_kernel(distances.reshape(1, -1))
-
-        #     # Update the affinity matrix for the current point
-        #     # affinity_matrix[i, :] = similarities
-
-        # affinity_matrix = 0.5 * (affinity_matrix + affinity_matrix.T)
 
         t, y, x, v, theta = data[:, 0], data[:, 1], data[:, 2], data[:, 3], data[:, 4]
         for i in range(num_points):
@@ -62,57 +48,64 @@ def predictWithK(testFeatures, numVessels, s, trainFeatures=None,
             later_data = data[later_time_indices]
             time_diff = later_data[:, 0] - t1
             acc_approx = np.where(time_diff != 0, (later_data[:, 3] - v1) / time_diff, 0)
-            x2_pred = x1 + np.sin(theta1) * (v1 * time_diff + 0.5 * acc_approx * time_diff**2)
-            y2_pred = y1 + np.cos(theta1) * (v1 * time_diff + 0.5 * acc_approx * time_diff**2)
+            x2_pred = x1 + np.sin(np.deg2rad(theta1 / 10)) * (v1 * time_diff + 0.5 * acc_approx * time_diff**2)
+            y2_pred = y1 + np.cos(np.deg2rad(theta1 / 10)) * (v1 * time_diff + 0.5 * acc_approx * time_diff**2)
             distances[i, later_time_indices] = np.sqrt((x2_pred - later_data[:, 2])**2 + (y2_pred - later_data[:, 1])**2)
-        distances = 0.5 * (distances + distances.T)
+        distances = distances + distances.T
         print(distances)
+        return distances
 
-        # for i in range(num_points):
-        #     for j in range(i + 1, num_points):
-        #         t1, y1, x1, v1, theta1 = data[i,:]
-        #         t2, y2, x2, v2, theta2 = data[j,:]
+    #     distances[distances == 0] = 1
+    #     affinity_matrix = np.minimum(1 / distances, 1)
+    #     affinity_matrix /= affinity_matrix.sum(axis=1, keepdims=True)
+    #     affinity_matrix = 0.5 * (affinity_matrix + affinity_matrix.T)
+    #     print(affinity_matrix)
+    #     return affinity_matrix
 
-        #         time_diff = t2 - t1
-        #         if time_diff == 0:
-        #             x2_pred = x1
-        #             y2_pred = y1
-        #         else:
-        #             acc_approx = (v2 - v1) / (time_diff)
-        #             x2_pred = x1 + math.cos(theta1) * (v1 * (time_diff) + 0.5 * acc_approx * math.pow(time_diff, 2))
-        #             y2_pred = y1 + math.sin(theta1) * (v1 * (time_diff) + 0.5 * acc_approx * math.pow(time_diff, 2))
-
-        #         distance = np.sqrt((x2_pred - x2)**2 + (y2_pred - y2)**2)
-
-        #         # Construct affinity graph
-        #         distances[i, j] = distances[j, i] = distance
-        
-        # affinity_matrix = rbf_kernel(distances, gamma=1.0 / (2.0 * 1.0**2))
-        # print(affinity_matrix)
-
-        # min_val = np.min(distances)
-        # max_val = np.max(distances)
-        # scaled_distances = (distances - min_val) / (max_val - min_val)
-        # affinity_matrix = np.ones_like(scaled_distances) - scaled_distances
-        # squared_distances = distances ** 2
-        # affinity_matrix = np.exp(-squared_distances / (2 * s**2))
-
-        distances[distances == 0] = 1
-        affinity_matrix = np.minimum(1 / distances, 1)
-        affinity_matrix /= affinity_matrix.sum(axis=1, keepdims=True)
-        affinity_matrix = 0.5 * (affinity_matrix + affinity_matrix.T)
-        print(affinity_matrix)
-        return affinity_matrix
 
 
     preprocessedTestFeatures = preprocess_data(testFeatures)
-    # Assuming testFeatures is the preprocessed unique data
-    affinity_matrix = construct_affinity_matrix(preprocessedTestFeatures, s=s)
+    distance_matrix = construct_distance_matrix(preprocessedTestFeatures)
+    print(np.shape(distance_matrix))
+    # distance_matrix = squareform(distance_matrix)
+    
+    linkage_matrix = linkage(distance_matrix, method="single")
+    predVessels = fcluster(linkage_matrix, numVessels, criterion="maxclust")
+    return predVessels
 
-    model = SpectralClustering(n_clusters=numVessels, affinity="precomputed")
-    pred_labels = model.fit_predict(affinity_matrix)
+    # model = SpectralClustering(n_clusters=numVessels, affinity="precomputed")
+    # predVessels = model.fit_predict(affinity_matrix)
 
-    return pred_labels
+
+    testFeatures = pd.DataFrame(testFeatures, columns=["SEQUENCE_DTTM", "LAT", "LON", "SPEED_OVER_GROUND", "COURSE_OVER_GROUND"])
+
+    # testFeatures["TIME_INTERVAL"] = testFeatures["SEQUENCE_DTTM"].diff()
+    # testFeatures.loc[0, "TIME_INTERVAL"] = 0
+    testFeatures["SPEED_X"] = testFeatures["SPEED_OVER_GROUND"] * np.sin(testFeatures["COURSE_OVER_GROUND"] / 10)
+    testFeatures["SPEED_Y"] = testFeatures["SPEED_OVER_GROUND"] * np.cos(testFeatures["COURSE_OVER_GROUND"] / 10)
+    testFeatures["ACC_X"] = testFeatures["SPEED_X"] / testFeatures["TIME_INTERVAL"]
+    testFeatures.loc[0, "ACC_X"] = 0
+    testFeatures["ACC_Y"] = testFeatures["SPEED_Y"] / testFeatures["TIME_INTERVAL"]
+    testFeatures.loc[0, "ACC_Y"] = 0
+
+    distances = np.zeros(len(testFeatures, testFeatures))
+    for i in range(len(testFeatures)):
+        for j in range(len(testFeatures)):
+            spatial_distance = haversine_distances(np.radians([[testFeatures.loc[i, "LAT"], testFeatures.loc[i, "LON"]]]),
+                                                   np.radians([[testFeatures.loc[j, "LAT"], testFeatures.loc[j, "LON"]]])).item()
+            temporal_distance = euclidean(testFeatures[i], testFeatures[j])
+            distances[i, j] = np.sqrt(spatial_distance ** 2 + temporal_distance ** 2)
+
+
+
+    print(testFeatures.loc[testFeatures["ACC_X"].isnull()])
+    print(testFeatures)
+    testFeatures = testFeatures.to_numpy()
+    # scaler = StandardScaler()
+    # testFeatures = scaler.fit_transform(testFeatures)
+    kmeans = KMeans(n_clusters=numVessels, init='k-means++', n_init=10, random_state=100)
+    predVessels = kmeans.fit_predict(testFeatures)
+    return predVessels
 
 def predictWithoutK(testFeatures, trainFeatures=None, trainLabels=None):
     # Unsupervised prediction, so training data is unused
@@ -137,15 +130,14 @@ if __name__ == "__main__":
     ari = -math.inf
     s_final = None
     # Prediction with specified number of vessels
-    for s in np.arange(0.001, 1, 0.001):
-        numVessels = np.unique(labels).size
-        predVesselsWithK = predictWithK(features, numVessels, s)
-        ariWithK = adjusted_rand_score(labels, predVesselsWithK)
-        if ariWithK > ari:
-            ariWithK = ari
-            s_final = s
-    print("ARI:", ari)
-    print("s final:", s_final)
+    numVessels = np.unique(labels).size
+    predVesselsWithK = predictWithK(features, numVessels)
+    ariWithK = adjusted_rand_score(labels, predVesselsWithK)
+    # if ariWithK > ari:
+    #     ariWithK = ari
+    #     s_final = s
+    # print("ARI:", ari)
+    # print("s final:", s_final)
     
     # Prediction without specified number of vessels
     predVesselsWithoutK = predictWithoutK(features)
