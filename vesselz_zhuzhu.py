af52219cf5678db5a00a1ebeb8b284403571e03f
KNguyen37 <ktn37@case.edu>
2023-12-06 20:32:13 -0500

    adding data

--- vesselz_zhuzhu.py
+++ vesselz_zhuzhu.py
@@ -32,6 +32,24 @@ def predictWithK(testFeatures, numVessels, trainFeatures=None,
     return predVessels
 
 def predictWithoutK(testFeatures, trainFeatures, trainLabels):
+    # Try range of K values
+    # Define ARI scoring
+    # ari_scorer = make_scorer(adjusted_rand_score)
+
+    # # Gridsearch cross-validation 
+    # kmeans = KMeans()
+    # param_grid = {'n_clusters': range(2, 31)}
+    # grid = GridSearchCV(kmeans, param_grid, cv=5, scoring=ari_scorer)
+
+    # grid.fit(trainFeatures, trainLabels)
+    # best_km = grid.best_estimator_
+    # best_num_clusters = best_km.n_clusters
+    
+    # return predictWithK(testFeatures, best_num_clusters)
+
+    # # Arbitrarily assume 20 vessels
+    # return predictWithK(testFeatures, 20, trainFeatures, trainLabels)
+###
     # Try different K values from 2 to 20
     k_range = range(15, 30)  
     elbow_k = find_elbow_k(trainFeatures, k_range) 
@@ -50,6 +68,7 @@ def find_elbow_k(train_features, k_range):
 
         sil_score = silhouette_score(train_features, kmeans.labels_)
         sil_scores.append(sil_score)
+
     # Find index of max difference 
     max_diff_idx = find_max_diff_index(sil_scores)
 
@@ -97,4 +116,45 @@ if __name__ == "__main__":
     plotVesselTracks(features[:,[2,1]], labels)
     plt.title('Vessel tracks by label')
     plt.show()
+# %%
+import numpy as np
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import StandardScaler
+from scipy.spatial.distance import cdist
+from sklearn.metrics.pairwise import haversine_distances
+from sklearn.neighbors import DistanceMetric
+import matplotlib.pyplot as plt
+
+from utils import loadData, plotVesselTracks
+# Sample data (replace this with your actual data)
+data = loadData('set1.csv')
+
+# Custom distance metric function for spatio-temporal clustering
+def custom_distance(X, Y):
+    spatial_distance = cdist(X[:, 3:5].reshape(-1,2), Y[:, 3:5].reshape(-1,2), metric='euclidean') # Convert to meters using Earth radius
+    direction_velocity_distance = cdist(X[:, 5:], Y[:, 5:], metric='euclidean')
+    time_distance = np.abs(X[:, 2] - Y[:, 2])  # Use absolute time difference
+    # Combine spatial, direction/velocity, and time distances
+    distance_matrix = spatial_distance + direction_velocity_distance + time_distance[:, np.newaxis]
+    return distance_matrix
+
+# Standardize the data
+scaler = StandardScaler()
+data_scaled = scaler.fit_transform(data)
+
+# Apply DBSCAN with custom spatio-temporal distance metric
+eps = 0.5  # The maximum distance between two samples for one to be considered as in the neighborhood of the other
+min_samples = 5  # The number of samples (or total weight) in a neighborhood for a point to be considered as a core point
+dbscan = DBSCAN(eps=eps, min_samples=min_samples, metric=custom_distance)
+labels = dbscan.fit_predict(data_scaled)
+
+# Add cluster labels to the original data
+data_with_labels = np.column_stack((data, labels))
+
+# Visualize the clusters (2D plot, you may need to adapt for 3D)
+plt.scatter(data_with_labels[:, 1], data_with_labels[:, 0], c=data_with_labels[:, -1])
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('Spatio-Temporal Clusters')
+plt.show()
 # %%
